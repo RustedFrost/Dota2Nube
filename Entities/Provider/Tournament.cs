833510aae921aee55e2d3d6b7ece289b8c6c861e
RustyFrost <jproelofse@gmail.com>
2015-06-14 13:26:36 +0200

    Initial

---
+++ Entities/Provider/Tournament.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Provider
+{
+    
+    public class Tournament
+    {
+        public Dota2NubeEntities db = new Dota2NubeEntities();
+        public List<Entities.Tournament> GetAllTournaments()
+        {
+            return db.Tournament.ToList();
+        }
+    }
+}
